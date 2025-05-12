49e46a393fe788d1f2fd18d9c92a2313b1e42d07
Julio Rafael <juliorafaelnft@hotmail.com>
2025-05-12 00:26:50 -0300

    db

--- db/migrate.ts
+++ db/migrate.ts
@@ -1,7 +1,7 @@
 import { config } from "dotenv";
-import { drizzle } from "drizzle-orm/postgres-js";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
-import postgres from "postgres";
+import db from "./db";
+
 
 config({
   path: ".env.local",
@@ -12,8 +12,6 @@ const runMigrate = async () => {
     throw new Error("POSTGRES_URL is not defined");
   }
 
-  const connection = postgres(process.env.POSTGRES_URL, { max: 1 });
-  const db = drizzle(connection);
 
   console.log("⏳ Running migrations...");
 
